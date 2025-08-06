0e902b317452412c92b454336abcdd15c2ea7c99
coouir <yoonsung.stu@gmail.com>
2025-08-06 11:10:37 +0900

    25_08_06_11:10

---
+++ 04_Neural Network Training/train_neuralnet.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from two_layer_net import TwoLayerNet
+from tensorflow.keras.datasets import mnist
+
+def _change_one_hot_label(X, num_classes=10):
+    return np.eye(num_classes)[X]
+
+def get_data(normalize=True, flatten=True, one_hot_label=False):
+    (x_train, t_train), (x_test, t_test) = mnist.load_data()
+    if normalize:
+        x_train = x_train.astype(np.float32) / 255.0
+        x_test = x_test.astype(np.float32) / 255.0
+    if flatten:
+        x_train = x_train.reshape(-1, 28 * 28)
+        x_test = x_test.reshape(-1, 28 * 28)
+    if one_hot_label:
+        t_train = _change_one_hot_label(t_train)
+        t_test = _change_one_hot_label(t_test)
+    return (x_train, t_train), (x_test, t_test)
+
+(x_train, t_train), (x_test, t_test) = get_data(normalize=True, flatten=True, one_hot_label=True)
+
+train_loss_list = []
+
+# 하이퍼파라미터
+iters_num = 10000 # 반복 횟수
+train_size = x_train.shape[0]
+batch_size = 100 # 미니배치 크기
+learning_rate = 0.1
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    # 미니배치 획득
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # 기울기 계산
+    grad = network.gradient(x_batch, t_batch)
+
+    # 매개변수 갱신
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    # 학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    print(i, loss)
+    train_loss_list.append(loss)
+
+# 그래프 그리기
+x = np.arange(len(train_loss_list))
+plt.plot(x, train_loss_list)
+plt.xlabel("iteration")
+plt.ylabel("loss")
+plt.ylim(0, 9)
+plt.xlim(0, 10000)
+plt.show()
